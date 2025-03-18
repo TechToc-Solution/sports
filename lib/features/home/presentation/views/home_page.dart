457157e5724d11305f50f9b0050c9b4d087a27f1
AHNXD <nzamlyalhady@gmail.com>
2025-03-18 15:23:33 +0400

    add drawer

--- lib/features/home/presentation/views/home_page.dart
+++ lib/features/home/presentation/views/home_page.dart
@@ -19,7 +19,34 @@ class _MyHomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(color: AppColors.primaryColors),
+              child: Text('Welcome'),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
+        iconTheme: const IconThemeData(color: AppColors.primaryColors),
         actions: [
           IconButton(
               onPressed: () {
